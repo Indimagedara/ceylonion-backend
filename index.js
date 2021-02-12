d90f4f1f01e60c8a1d6ca62c0e8e6cff7acec9e2
indimagedara <nimeshnishara@gmail.com>
2021-02-13 01:44:41 +0530

    App initiated

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 8000;
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}..`)
+});
+
+app.get('/',(req,res)=>{
+    console.log('You are not allowed to be here');
+    res.send('You are not allowed to be here');
+})
